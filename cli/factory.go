7fe616b056910af8cf46a73b7d91259aeffe3a7a
Roman Molochkov <molochkov2017@gmail.com>
2023-04-22 19:53:31 +0300

    refactor: rewrite comment

--- cli/factory.go
+++ cli/factory.go
@@ -261,7 +261,7 @@ func (b *clientFactory) createTLSConfig(c *cli.Context) (*tls.Config, error) {
 		tlsConfig := auth.NewTLSConfigForServer(host, !disableHostNameVerification)
 		return tlsConfig, nil
 	}
-	// If we are given a TLS flag, set the TLS server name from address for DNS resolution
+	// If we are given a TLS flag, set the TLS server name from the address
 	if enableTLS {
 		hostPort := c.String(FlagAddress)
 		if hostPort == "" {
